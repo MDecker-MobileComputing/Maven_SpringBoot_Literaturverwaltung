1cc64b88ff06aca3dce21cdef589ecd9bfdc7089
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-06-30 10:26:45 +0200

    neu: detail-thema (Anzeige Schlagwoerter tut noch nicht)

--- src/main/java/de/eldecker/dhbw/spring/literaturverwaltung/db/ThemaEntity.java
+++ src/main/java/de/eldecker/dhbw/spring/literaturverwaltung/db/ThemaEntity.java
@@ -16,29 +16,29 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 
 
 /**
- * Entity-Klasse für wiss. Themen, denen Publikationen zugeordnet werden. 
+ * Entity-Klasse für wiss. Themen, denen Publikationen zugeordnet werden.
  */
 @Entity
 @Table(name = "THEMA")
 public class ThemaEntity extends AbstractPersistable<Long> {
 
-    /** Wissenschaftliches Thema, z.B. "Workflow Management" */ 
+    /** Wissenschaftliches Thema, z.B. "Workflow Management" */
     private String thema;
-    
+
     /**
-     * Schlagwörter für dieses Thema; diese werden in der Tabelle 
+     * Schlagwörter für dieses Thema; diese werden in der Tabelle
      * {@code SCHLAGWOERTER} abgelegt, welche die Fremdschlüsselspalte
      * {@code THEMA_ID} hat.
      */
     @ElementCollection
-    @CollectionTable( name = "schlagwoerter",
+    @CollectionTable( name        = "schlagwoerter",
                       joinColumns = @JoinColumn( name="thema_id") )
     private Set<SchlagwortEmbeddable> schlagwoerterSet = new HashSet<>();
-    
+
     /** Publikationen, die dieses Thema haben. */
     @ManyToMany(mappedBy = "themen")
     private Set<AbstractPublikationEntity> publikationen;
-    
+
     /** Konstruktor für JPA */
     public ThemaEntity() {}
 
@@ -46,56 +46,58 @@ public class ThemaEntity extends AbstractPersistable<Long> {
      * Konstruktor, um skalare Attribute zu setzen.
      */
     public ThemaEntity( String thema ) {
-        
+
         this.thema = thema;
     }
-    
+
     /**
-     * Konstruktor um alle Non-ID-Attribute zu setzen. 
+     * Konstruktor um alle Non-ID-Attribute zu setzen.
      */
     public ThemaEntity( String thema, SchlagwortEmbeddable... schlagwoerter ) {
-        
+
         this.thema = thema;
-        List<SchlagwortEmbeddable> schlagwortListe = asList( schlagwoerter ); 
+        List<SchlagwortEmbeddable> schlagwortListe = asList( schlagwoerter );
         this.schlagwoerterSet.addAll( schlagwortListe );
     }
-    
+
     public String getThema() {
-        
+
         return thema;
     }
 
     public void setThema( String thema ) {
-        
+
         this.thema = thema;
     }
 
     public Set<SchlagwortEmbeddable> getSchlagwoerterSet() {
-        
+
         return schlagwoerterSet;
     }
 
     public void setSchlagwoerterSet( Set<SchlagwortEmbeddable> schlagwoerterSet ) {
-        
+
         this.schlagwoerterSet = schlagwoerterSet;
-    }            
-    
+    }
+
+
     /**
-     * Hilfsmethode, um der Menge der Schlagworte ein Thema hinzuzufügen. 
+     * Hilfsmethode, um der Menge der Schlagworte ein Thema hinzuzufügen.
      */
     public void addSchlagwort( SchlagwortEmbeddable schlagwort ) {
-        
-        schlagwoerterSet.add(schlagwort);        
+
+        schlagwoerterSet.add(schlagwort);
     }
-    
+
+
     /**
      * Methode gibt nur Thema zurück.
-     * 
-     * @return Wert von {@link #getThema()}
+     *
+     * @return Wert von {@link #getThema()}, z.B. "Workflow Management"
      */
     @Override
     public String toString() {
-        
+
         return getThema();
     }
 
--- src/main/java/de/eldecker/dhbw/spring/literaturverwaltung/web/ThymeleafController.java
+++ src/main/java/de/eldecker/dhbw/spring/literaturverwaltung/web/ThymeleafController.java
@@ -1,5 +1,7 @@
 package de.eldecker.dhbw.spring.literaturverwaltung.web;
 
+import static java.lang.String.format;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import de.eldecker.dhbw.spring.literaturverwaltung.db.AbstractPublikationEntity;
+import de.eldecker.dhbw.spring.literaturverwaltung.db.ThemaEntity;
 import de.eldecker.dhbw.spring.literaturverwaltung.db.repos.PublikationenRepo;
+import de.eldecker.dhbw.spring.literaturverwaltung.db.repos.ThemenRepo;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,65 +22,102 @@ import org.slf4j.LoggerFactory;
 
 
 /**
- * Klasse mit Controller-Methoden für Thymeleaf-Templates. 
+ * Klasse mit Controller-Methoden für Thymeleaf-Templates.
  */
 @Controller
 @RequestMapping( "/app" )
 public class ThymeleafController {
 
     private static final Logger LOG = LoggerFactory.getLogger( ThymeleafController.class );
-    
+
     /** Repo-Bean für Zugriff auf alle Publikationen */
     @Autowired
     private PublikationenRepo _publikationenRepo;
-    
-    
+
+    /** Repo-Bean für Zugriff auf alle Themen */
+    @Autowired
+    private ThemenRepo _themenRepo;
+
+
     /**
      * Seite mit allen Publikationen sortiert nach Jahreszahl zurückgeben.
-     * 
+     *
      * @param model Objekt für Platzhalterwerte in Template
-     * 
+     *
      * @return Name der Template-Datei {@code liste-alle-publikationen.html}
      */
     @GetMapping( "/liste-alle-publikationen" )
     public String allePublikationen( Model model ) {
-        
+
         List<AbstractPublikationEntity> liste = _publikationenRepo.findAllByOrderByJahrAsc();
-        
+
         model.addAttribute( "liste", liste );
-        
+
         return "liste-alle-publikationen";
     }
-    
-    
+
+
     /**
      * Seite mit Detailansicht anzeigen.
-     * 
+     *
      * @param id Pfadparameter mit ID der Publikation
-     * 
+     *
      * @param model Objekt für Platzhalterwerte in Template
-     * 
+     *
      * @return Name der Template-Datei {@code detail-publikation.html}
      */
     @GetMapping( "/publikation/{id}" )
-    public String allePublikationen( @PathVariable("id") Long id,
-                                     Model model ) {
-        
-        final Optional<AbstractPublikationEntity> publikationOptional = _publikationenRepo.findById( id );
+    public String detailsPublikation( @PathVariable("id") Long id,
+                                      Model model ) {
+
+        final Optional<AbstractPublikationEntity> publikationOptional =
+                                                    _publikationenRepo.findById( id );
         if ( publikationOptional.isEmpty() ) {
-            
-            String fehlertext = String.format( "Keine Publikation mit ID=%d gefunden.", id );
-            
+
+            String fehlertext = format( "Keine Publikation mit ID=%d gefunden.", id );
+
             LOG.warn( fehlertext );
-            
+
             model.addAttribute( "fehlermeldung", fehlertext );
             return "fehlerseite";
         }
-        
-        AbstractPublikationEntity publikation = publikationOptional.get();
+
+        final AbstractPublikationEntity publikation = publikationOptional.get();
         model.addAttribute( "publikation", publikation );
-        
+
         return "detail-publikation";
     }
-    
+
+
+    /**
+     * Seite mit Detailansicht für ein Thema anzeigen.
+     *
+     * @param id Pfadparameter mit ID des Themas
+     *
+     * @param model Objekt für Platzhalterwerte in Template
+     *
+     * @return Name der Template-Datei {@code detail-thema.html}
+     */
+    @GetMapping( "/thema/{id}" )
+    public String detailsThema( @PathVariable("id") Long id,
+                                Model model ) {
+
+        final Optional<ThemaEntity> themaOptional = _themenRepo.findById( id );
+        if ( themaOptional.isEmpty() ) {
+
+            String fehlertext = format( "Kein Thema mit ID=%d gefunden.", id );
+
+            LOG.warn( fehlertext );
+
+            model.addAttribute( "fehlermeldung", fehlertext );
+            return "fehlerseite";
+        }
+
+        final ThemaEntity thema = themaOptional.get();
+
+        model.addAttribute( "thema", thema );
+
+        return "detail-thema";
+    }
+
 }
